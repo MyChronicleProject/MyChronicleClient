a53cac99186829c5ae58e9acfb0b375791ef1895
Aleksandra Starowicz <as307323@student.polsl.pl>
2025-01-02 16:42:30 +0100

    Poprawienie

--- src/component/TreeView.tsx
+++ src/component/TreeView.tsx
@@ -89,6 +89,43 @@ export default function TreeView() {
     }
   };
 
+  //  const handleEdgeClick = (event: React.MouseEvent, edge: any) => {
+  //     console.log("Kliknięto relację o ID:", edge);
+  //     if (edge.target.includes("*")) {
+  //       const [id1, id2] = edge.target.split("*");
+
+  //       const clickedRelation = relation.find(
+  //         (rel) =>
+  //           (rel.personId_1 === id1 && rel.personId_2 === id2) ||
+  //           (rel.personId_1 === id2 && rel.personId_2 === id1)
+  //       );
+  //       console.log(clickedRelation);
+  //       onEdgeClick([clickedRelation, null]);
+  //     } else if (edge.source.includes("*")) {
+  //       const [id1, id2] = edge.source.split("*");
+  //       console.log(id1, id2);
+  //       const clickedRelation1 = relation.find(
+  //         (rel) =>
+  //           (rel.personId_1 === id1 && rel.personId_2 === edge.target) ||
+  //           (rel.personId_1 === edge.target && rel.personId_2 === id1)
+  //       );
+  //       const clickedRelation2 = relation.find(
+  //         (rel) =>
+  //           (rel.personId_1 === id2 && rel.personId_2 === edge.target) ||
+  //           (rel.personId_1 === edge.target && rel.personId_2 === id2)
+  //       );
+  //       console.log(clickedRelation1, clickedRelation2);
+  //       onEdgeClick([clickedRelation1, clickedRelation2]);
+  //     } else {
+  //       const clickedRelation = relation.find(
+  //         (rel) =>
+  //           (rel.personId_1 === edge.source && rel.personId_2 === edge.target) ||
+  //           (rel.personId_1 === edge.target && rel.personId_2 === edge.source)
+  //       );
+  //       onEdgeClick([clickedRelation, null]);
+  //     }
+  //   };
+
   const convertTextToLabel = (text: string): JSX.Element => {
     const parts = text.split("\\n").map((part, index) => {
       return (
@@ -125,6 +162,20 @@ export default function TreeView() {
           />
         </div>
       )}
+      {/* {visibleRelation && (
+                <div className="person-panel">
+                  <RelationDetail
+                    selectedEdgeId={selectedEdge[0] ? selectedEdge[0] : null}
+                  />
+                </div>
+              )}
+              {visibleRelation2 && (
+                <div className="person-panel">
+                  <RelationDetail
+                    selectedEdgeId={selectedEdge[1] ? selectedEdge[1] : null}
+                  />
+                </div>
+              )} */}
 
       {/* <button onClick={() => saveTreeToFile()}>Save</button>
   <button onClick={downloadPDF}>Download PDF</button> */}
