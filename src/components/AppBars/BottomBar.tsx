cf088535ef4363ffd8e5628ad95a6a8a0db1277e
Karolina <k4aro0@gmail.com>
2025-01-27 18:20:56 +0100

    email button

--- src/components/AppBars/BottomBar.tsx
+++ src/components/AppBars/BottomBar.tsx
@@ -1,14 +1,13 @@
-import { Button, Container, Menu } from "semantic-ui-react";
-import { NavLink } from "react-router-dom";
+import { Button } from "semantic-ui-react";
 import "../../Styles/appBarStyle.css";
 
 export default function BottomBar() {
   return (
     <div className="appBarBottom">
       <p className="contact-text">Skontaktuj się z nami! </p>
-      <Button as={NavLink} to="/settingsPage" className="username-button">
-        Contact email
-      </Button>
+      <Button className="username-button">
+        <a href="mailto:mychronicleofficial@gmail.com">Contact Email</a>
+      </Button>|
     </div>
   );
 }
